2e3de0b3b06b18891c16354250fbf16a642d322e
Max <mfong415@gmail.com>
2023-01-10 10:14:33 -0800

    Began working on reviews

--- frontend/src/components/PastOrders/index.jsx
+++ frontend/src/components/PastOrders/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import BundleModals from '../universalModals/BundleModals'
 import UXHeader from '../UXHeader'
-
+import './PastOrders.css'
+import { useSelector } from 'react-redux'
 const PastOrders = () => {
+
+  // const transactions = useSelector(getPastTransactions)
+  let transactions
+
   return (
     <>
       <UXHeader/>
-      <div>HERE ARE ALL YOUR PAST ORDERS</div>
+      <h1 className='univ-padding orders-header'>Past Orders</h1>
+      {transactions.map((transaction,idx) => (
+      <Transaction transaction={transaction} key={idx}/>)
+
+        )}
       <BundleModals/>
     </>
   )
