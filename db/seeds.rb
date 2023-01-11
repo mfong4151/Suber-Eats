7090e76d80169f88b5e70b05b9570c38413e2a7e
Max <mfong415@gmail.com>
2023-01-10 20:51:37 -0800

    bug fixes for render

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@ require "open-uri"
 class_names = [User, Restaurant, Menu, MenuItem, Review, Cart, Transaction]
 table_strings = ['users', 'restaurants', 'menus', 'menu_items', 'reviews', 'carts', 'transactions']
 EasySeeds.destroy_tables(class_names, table_strings)
-# EasySeeds.create_easy_seed_data(class_names)
+EasySeeds.create_easy_seed_data(class_names)
 
 
-#Used to test delete, update methods
-tables, table_strings = EasySeeds.tables_from_csvs()
-EasySeeds.single_seeder(tables[1], Restaurant, table_strings[1])
+# #Used to test delete, update methods
+# tables, table_strings = EasySeeds.tables_from_csvs()
+# EasySeeds.single_seeder(tables[1], Restaurant, table_strings[1])
 # Restaurant.first.image.attach(io: URI.open('https://s3-media0.fl.yelpcdn.com/bphoto/C3C_Rgzz-h3CZGg6IdTFoQ/l.jpg'), filename: 'champa-garden.jpg')
