a0a196292fd29ab2443ceb82219a5a9fd9e1dcaf
Max <mfong415@gmail.com>
2023-01-10 21:27:28 -0800

    continued bug fixes

--- db/migrate/20221230161702_create_menu_items.rb
+++ db/migrate/20221230161702_create_menu_items.rb
@@ -4,7 +4,6 @@ class CreateMenuItems < ActiveRecord::Migration[7.0]
     create_table :menu_items do |t|
       t.string :item_name, null:false
       t.float :price, null:false
-      t.string :header
       t.references :menu, null:false, index:true, foreign_key:{to_table: :menus}
       t.timestamps
     end
