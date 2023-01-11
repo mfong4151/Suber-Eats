da46ecadb6937b9666b3cb52de0ed43469da8d50
Max <mfong415@gmail.com>
2023-01-10 20:42:08 -0800

    debbugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require "open-uri"
   
 class_names = [User, Restaurant, Menu, MenuItem, Review, Cart, Transaction]
 table_strings = ['users', 'restaurants', 'menus', 'menu_items', 'reviews', 'carts', 'transactions']
-EasySeeds.destory_tables(class_names, table_strings)
+EasySeeds.destroy_tables(class_names, table_strings)
 EasySeeds.create_easy_seed_data(class_names)
 
 
