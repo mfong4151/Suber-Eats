75dc7a079f111809e18ee85a4fea07d5f84fb87e
Max <mfong415@gmail.com>
2023-01-11 21:25:31 -0800

    More google maps edits, implemented aws images

--- frontend/src/components/UserExperiencePage/UXPickup/Options/OptionsGridItem.jsx
+++ frontend/src/components/UserExperiencePage/UXPickup/Options/OptionsGridItem.jsx
@@ -3,15 +3,16 @@ import { NavLink } from 'react-router-dom';
 import "../Pickup.css"
 const OptionsGridItem = ({restaurant}) => {
 
+  console.log(restaurant)
   const timeDistance = '5-15 min 0.4mi';
   
   return (
     <div className="options-listing">
       <NavLink to={`/restaurantListing/${restaurant.id}`}>
         <div className='options-body'>
-         {/* <img className='options-image' src={listing.photo} /> */}
+         <img className='options-image' src={restaurant.imageUrl} />
 
-        <h2 className='restaurant-name'>{restaurant.name}</h2>
+        <h3 className='restaurant-name' id="rest-name">{restaurant.name}</h3>
         <div className='listing-time-distance'><img src="" />{timeDistance}</div>
       </div>
       </NavLink>
