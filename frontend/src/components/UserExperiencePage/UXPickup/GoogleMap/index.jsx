c75e7299b77ca443067b449dd2af62aab4163f00
Max <mfong415@gmail.com>
2023-01-11 18:27:03 -0800

    Finished crud feature

--- frontend/src/components/UserExperiencePage/UXPickup/GoogleMap/index.jsx
+++ frontend/src/components/UserExperiencePage/UXPickup/GoogleMap/index.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {GoogleMap, useLoadScript, Marker} from "@react-google-maps/api";
 import "./GoogleMap.css"
 import { SharedContext } from '../../../../App';
 import { useContext } from 'react';
-//map id for the styled map we made c9833b310ffc337b
+
 const Map = ({restaurants}) => {
   const { isLoaded} = useLoadScript({googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY
   })
 
-  if(!isLoaded) return(<h1>loading...</h1>)
 
-  const coords = {
-    lat: 37.72988998953665, 
-    lng: -122.45906484395803
-    }
+  const coords = useMemo(()=>({lat: 37.72988998953665, lng: -122.45906484395803}), []);
+  const options = useMemo(()=>({
+        mapId: '73cef3161f877bcd',
+        disableDefaultUI:true,
+        }),[]);
+  
+  
 
-  return <GoogleMap zoom={15} center={coords} mapContainerClassName="map-container"></GoogleMap>
+
+  return (
+    <GoogleMap 
+      zoom={15} 
+      center={coords} 
+      mapContainerClassName="map-container"
+      options={options}
+      ></GoogleMap>)
 }
 
 export default Map;
