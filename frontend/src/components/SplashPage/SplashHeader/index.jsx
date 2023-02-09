4f27451e98dc7e9a2636fdee72f838715eda3847
Max <mfong415@gmail.com>
2023-02-09 08:26:35 -0800

    Fixed header spacing

--- frontend/src/components/SplashPage/SplashHeader/index.jsx
+++ frontend/src/components/SplashPage/SplashHeader/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "../SplashPage.css"
 import {useState} from 'react'
 import {useHistory} from 'react-router-dom'
-
+import MenuIcon from '../../SVGs/MenuIcon'
 const SplashHeader = ({menuModal, setMenuModal}) => {
   const history = useHistory();
 
@@ -14,15 +14,17 @@ const SplashHeader = ({menuModal, setMenuModal}) => {
   return (
      <header id="splash-header" className="univ-padding">
 
-            <div className="logo header-left">
-                <div id='splash-hamburger' onClick={()=> setMenuModal(!menuModal)}>
-                  <svg width="22" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-                </div>
-                <div>
-                  <h1 className='suber'>Suber <span className="eats">Eats</span></h1>
-                </div>
-            </div>
+        <div className="logo header-left" id='splash-header-left'>
+              <button className='menu-modal minimal-header-padding'>
+                <MenuIcon/>
+              </button>
+      
+              <div id='logo-holder'>
+                    <h1 className='suber'>Suber <span className="eats">Eats</span></h1>
+              </div>
+
+        </div>
+        
 
             {/* <form>
               <input type='text' name='location' placeholder='Enter pickup address'/> 
