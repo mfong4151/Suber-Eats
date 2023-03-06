e49e7e4ae66e4ec279fa3c1be9d3931e235a4225
Max <mfong415@gmail.com>
2023-03-06 11:13:36 -0800

    Code cleanup

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -37,15 +37,9 @@ class Restaurant < ApplicationRecord
     end
   
     def self.get_dist_from_user(user_loc, restaurant)
-      
      (1000 *Math.sqrt((user_loc.longitude - restaurant.longitude) ** 2 + (user_loc.latitude - restaurant.latitude) ** 2)).to_i
     end
 
-    def avg_dish_price
-      menu_items = self.menu_items
-      return 7 if menu_items.length == 0
-      (menu_items.sum{|item| item.price}/menu_items.length).to_i
-    end
     
     has_one :menu,
     foreign_key: :restaurant_id,
