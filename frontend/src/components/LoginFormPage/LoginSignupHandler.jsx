dd5aadb289e366acf687bbbf15fc2bfeac0dc82c
Max <mfong415@gmail.com>
2023-03-10 09:41:12 -0800

    Code cleanup

--- frontend/src/components/LoginFormPage/LoginSignupHandler.jsx
+++ frontend/src/components/LoginFormPage/LoginSignupHandler.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react'
-import { useState, useContext } from 'react';
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from '../../store/user';
 import PasswordEntry from './PasswordEntry';
 import SignUpEntry from './SignUpEntry';
-import { useSelector } from 'react-redux';
 import { getUsers } from '../../store/user';
 import { aggregateUserCredentials } from './utils/compileUserSet';
 
