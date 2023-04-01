006cf3de131954d2723345fb2e1202914226ef34
Max <mfong415@gmail.com>
2023-03-31 18:36:54 -0700

    Code cleanup

--- frontend/src/components/SplashPage/SplashHeader.jsx
+++ frontend/src/components/SplashPage/SplashHeader.jsx
@@ -22,19 +22,16 @@ const SplashHeader = ({modalStates}) => {
               <div id='logo-holder'>
                     <h1 className='suber'>Suber <span className="eats">Eats</span></h1>
               </div>
-
         </div>
         
 
-
-            <div id="user-auth-buttons">
-                <button className="btn-round header-btn-round grey-button" onClick={goToLogin}>
-                  <svg aria-hidden="true" focusable="false" viewBox="0 0 26 26" className='button-icon'><path fillRule="evenodd" clipRule="evenodd" 
-                    d="M18.958 7.042a5.958 5.958 0 11-11.916 0 5.958 5.958 0 0111.916 0zM3.25 21.667c0-3.575 2.925-6.5 6.5-6.5h6.5c3.575 0 6.5 2.925 6.5 6.5v3.25H3.25v-3.25z"></path>
-                  </svg>
-                  Log In</button>
-                <button className='btn-round header-btn-round black-button' onClick={goToLogin}>Sign Up</button>
-            </div>
+        <div id="user-auth-buttons">
+            <button className="btn-round header-btn-round grey-button" onClick={goToLogin}>
+              
+              Log In
+              </button>
+            <button className='btn-round header-btn-round black-button' onClick={goToLogin}>Sign Up</button>
+        </div>
 
        </header>
   )
