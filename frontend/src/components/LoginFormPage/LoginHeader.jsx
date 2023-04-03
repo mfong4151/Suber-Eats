1d9abb69193e6cf095c978dbca46cb0548cbb543
Max <mfong415@gmail.com>
2023-04-03 14:46:41 -0700

    Refactored login page for mobile

--- frontend/src/components/LoginFormPage/LoginHeader.jsx
+++ frontend/src/components/LoginFormPage/LoginHeader.jsx
@@ -3,11 +3,12 @@ import { useHistory } from 'react-router-dom'
 import './LoginFormPage.css'
 
 const LoginHeader = () => {
-  const history = useHistory()
+  const history = useHistory();
+
   return (
     <header id="login-suber-eats">
         <div className="logo login-logo" onClick={()=> history.push('/')}>
-            <h1 className='suber'>Suber <span className="eats eats-green">Eats</span></h1>
+            <h1 className='suber header-text'>Suber <span className="eats eats-green">Eats</span></h1>
         </div>
 
     </header>
