1d0c9bef8893b9146a401cccd29cded3c7f66fc8
Max <mfong415@gmail.com>
2023-04-03 18:55:26 -0700

    Bare minimum views page optimized for mobile

--- frontend/src/components/UserExperiencePage/UXView/Options/OptionsGridItem.jsx
+++ frontend/src/components/UserExperiencePage/UXView/Options/OptionsGridItem.jsx
@@ -5,8 +5,8 @@ const OptionsGridItem = ({restaurant}) => {
   
   const timeDistance = 'In your area';
   return (
-    <div className="options-listing">
-      <NavLink to={`/restaurantListing/${restaurant.id}`}>
+    <>
+  <NavLink to={`/restaurantListing/${restaurant.id}`}>
         <div className='options-body'>
          {/* //comment out the below line and comment in the one below itfor temporarily disabling pictures */}
          <img className='options-image' src={restaurant.imageUrl} />
@@ -14,14 +14,14 @@ const OptionsGridItem = ({restaurant}) => {
 
         <div className='info-container'>
           <div>
-             <h3 className='restaurant-name' id="rest-name">{restaurant.name}</h3>
+             <h3 className='restaurant-name'>{restaurant.name}</h3>
               <div className='listing-time-distance'>{timeDistance}</div>
               </div>
           <span className='grey-button-no-shad rating-button udc'>{restaurant.rating}</span>
         </div>
-      </div>
+        </div>
       </NavLink>
-    </div>
+      </>
   )
 }
 
