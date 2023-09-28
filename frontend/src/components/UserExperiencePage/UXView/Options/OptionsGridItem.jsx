30096a969a90fa7e4c02c1d136f1cd09b687ea1d
Max <mfong415@gmail.com>
2023-09-28 07:52:46 -0700

    Fixed gallery styling

--- frontend/src/components/UserExperiencePage/UXView/Options/OptionsGridItem.jsx
+++ frontend/src/components/UserExperiencePage/UXView/Options/OptionsGridItem.jsx
@@ -3,11 +3,10 @@ import { NavLink } from 'react-router-dom';
 import "../Pickup.css"
 const OptionsGridItem = ({ restaurant }) => {
 
-  const timeDistance = 'In your area';
   return (
     <>
       <NavLink to={`/restaurantListing/${restaurant.id}`}>
-        <div className='options-body'>
+        <div className='options-body udc fdc'>
           {/* //comment out the below line and comment in the one below itfor temporarily disabling pictures */}
           <img className='options-image' src={restaurant.imageUrl} />
           {/* <img className='options-image' src={null} /> */}
@@ -15,7 +14,7 @@ const OptionsGridItem = ({ restaurant }) => {
           <div className='info-container'>
             <div>
               <h3 className='restaurant-name'>{restaurant.name}</h3>
-              <div className='listing-time-distance'>{timeDistance}</div>
+              <div className='listing-time-distance'>In your area</div>
             </div>
             <span className='grey-button-no-shad rating-button udc'>{restaurant.rating}</span>
           </div>
