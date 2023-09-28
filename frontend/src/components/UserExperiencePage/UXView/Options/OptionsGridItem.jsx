9b30d77f2aff3c71f3a5c2d31ddc3bf890857b43
Max <mfong415@gmail.com>
2023-09-28 07:23:00 -0700

    Line formatting

--- frontend/src/components/UserExperiencePage/UXView/Options/OptionsGridItem.jsx
+++ frontend/src/components/UserExperiencePage/UXView/Options/OptionsGridItem.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import "../Pickup.css"
-const OptionsGridItem = ({restaurant}) => {
-  
+const OptionsGridItem = ({ restaurant }) => {
+
   const timeDistance = 'In your area';
   return (
     <>
-  <NavLink to={`/restaurantListing/${restaurant.id}`}>
+      <NavLink to={`/restaurantListing/${restaurant.id}`}>
         <div className='options-body'>
-         {/* //comment out the below line and comment in the one below itfor temporarily disabling pictures */}
-         <img className='options-image' src={restaurant.imageUrl} />
-         {/* <img className='options-image' src={null} /> */}
+          {/* //comment out the below line and comment in the one below itfor temporarily disabling pictures */}
+          <img className='options-image' src={restaurant.imageUrl} />
+          {/* <img className='options-image' src={null} /> */}
 
-        <div className='info-container'>
-          <div>
-             <h3 className='restaurant-name'>{restaurant.name}</h3>
+          <div className='info-container'>
+            <div>
+              <h3 className='restaurant-name'>{restaurant.name}</h3>
               <div className='listing-time-distance'>{timeDistance}</div>
-              </div>
-          <span className='grey-button-no-shad rating-button udc'>{restaurant.rating}</span>
-        </div>
+            </div>
+            <span className='grey-button-no-shad rating-button udc'>{restaurant.rating}</span>
+          </div>
         </div>
       </NavLink>
-      </>
+    </>
   )
 }
 
